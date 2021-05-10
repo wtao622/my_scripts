390ba9ff78eb845497af6f534b9cf2554c5b5a68
lpssxs <731588359@qq.com>
2021-05-10 19:17:43 +0800

    11

--- shell/jd/lpss_diy.sh
+++ shell/jd/lpss_diy.sh
@@ -42,9 +42,9 @@ for npc_scripts in my_scripts_list
         eval sc_result=$(cat /jd/config/crontab.list | grep "${npc_scripts}")
 
             if [[ "$sc_result" != "" ]];then
-                echo -e "$scripts 脚本计划任务已存在，无需更新～ \n"
+                echo -e "${scripts} 脚本计划任务已存在，无需更新～ \n"
             else
-                echo -e "$scripts 脚本计划任务不存在，正在更新～ \n"
+                echo -e "${scripts} 脚本计划任务不存在，正在更新～ \n"
 
                 #遍历 my_cron_list 添加对应脚本的 cron 
 
@@ -52,12 +52,12 @@ for npc_scripts in my_scripts_list
                 do
                     eval cr_result=$(echo $npc_cron | grep "${npc_scripts}")
                         if [[ "$cr_result" != "" ]];then
-                            echo -e "$scripts 脚本计划任务正在添加～ \n"
+                            echo -e "${scripts} 脚本计划任务正在添加～ \n"
                             echo "$npc_cron" >> /jd/config/crontab.list
                             crontab /jd/config/crontab.list
-                            echo -e "$scripts 脚本添加完成～ \n"
+                            echo -e "${scripts} 脚本添加完成～ \n"
                         else
-                            echo -e "请在 my_cron_list 添加该脚本的计划任务～ \n"
+                            echo -e "请在 my_cron_list 添加 ${scripts} 脚本的计划任务～ \n"
                         fi 
                 done
 
@@ -73,8 +73,9 @@ cp -rf  /jd/home.html /jd/panel/public
 echo -e "Home cookies 按钮修复完成!!!\n"
 
 #执行完毕后自动更新 lpss_diy.sh 
+echo -e "自动更新 lpss_diy.sh 中～～～ \n"
 wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/shell/jd/lpss_diy.sh -O /jd/config/lpss_diy.sh && chmod +x /jd/config/lpss_diy.sh
-
+echo -e "lpss_diy.sh 完成，感谢使用～ \n"
 
 
 
