4957413b61ee2a7223baebbe9c7593cd7457ca5a
lpssxs <731588359@qq.com>
2021-05-11 15:26:12 +0800

    super_2

--- shell/jd/lpss_diy.sh
+++ shell/jd/lpss_diy.sh
@@ -16,7 +16,7 @@ wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_sc
 wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/js_code/jd/jd_half_redrain.js -O /jd/scripts/jd_half_redrain.js
 wget -q --no-check-certificate https://raw.githubusercontent.com/monk-coder/dust/dust/car/adolf_ETIP.js -O /jd/scripts/jd_adolf_ETIP.js
 wget -q --no-check-certificate https://jdsharedresourcescdn.azureedge.net/jdresource/jd_syj.js -O /jd/scripts/jd_syj_2.js 
-
+wget -q --no-check-certificate https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js  -O /jd/scripts/jd_super_redrain_2.js 
 #添加需要添加的脚本 name
 
 my_cron_file="/jd/config/crontab.list"
@@ -28,6 +28,7 @@ jd_half_redrain
 jd_unsubscribe_2
 jd_adolf_ETIP
 jd_syj_2
+jd_super_redrain_2
 "
 
 #添加需要添加脚本的 cron
@@ -37,6 +38,7 @@ jd_half_redrain="30 20-23/1 * * * jd jd_half_redrain"
 jd_unsubscribe_2="45 22 * * * jd jd_unsubscribe_2"
 jd_adolf_ETIP="15 8 9-31 5 * jd jd_adolf_ETIP"
 jd_syj_2="10 0,7,23 * * * jd jd_syj_2"
+jd_super_redrain_2="0 0-23/1 * * * jd jd_super_redrain_2"
 
 
 #添加需要删除的脚本的 name
