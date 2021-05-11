9fb9299a883eec8377cb7496f616f5476f726973
lpssxs <731588359@qq.com>
2021-05-11 23:54:31 +0800

    apply normal

--- shell/jd/lpss_diy.sh
+++ shell/jd/lpss_diy.sh
@@ -8,6 +8,7 @@
 #赋予脚本执行权限 chmod +x /jd/config/lpss_diy.sh
 #建议手动添加以下计划任务 */10 * * * * bash /jd/config/lpss_diy.sh >> /jd/log/lpss_diy.log 2>&1
 
+
 # 下载需要添加的脚本 link 
 
 #wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/html/home.html  -O /jd/home.html
@@ -16,12 +17,13 @@ wget -q --no-check-certificate  https://raw.githubusercontent.com/forpw2009/my_s
 wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/js_code/jd/jd_super_redrain.js -O /jd/scripts/jd_super_redrain.js
 wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/js_code/jd/jd_half_redrain.js -O /jd/scripts/jd_half_redrain.js
 wget -q --no-check-certificate https://raw.githubusercontent.com/monk-coder/dust/dust/car/adolf_ETIP.js -O /jd/scripts/jd_adolf_ETIP.js
-wget -q --no-check-certificate https://jdsharedresourcescdn.azureedge.net/jdresource/jd_syj.js -O /jd/scripts/jd_syj_2.js 
-wget -q --no-check-certificate https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js  -O /jd/scripts/jd_super_redrain_2.js 
+wget -q --no-check-certificate https://jdsharedresourcescdn.azureedge.net/jdresource/jd_syj.js -O /jd/scripts/jd_syj.js 
+wget -q --no-check-certificate https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js  -O /jd/scripts/jd_npc_redrain.js 
+wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/js_code/jd/jd_daily_lottery.js -O /jd/scripts/jd_daily_lottery.js 
 
 
 my_cron_file="/jd/config/crontab.list"
-
+chmod 666 $my_cron_file
 
 #添加需要添加的脚本 name
 my_scripts_list_add="
@@ -30,8 +32,9 @@ jd_super_redrain
 jd_half_redrain
 jd_unsubscribe_2
 jd_adolf_ETIP
-jd_syj_2
-jd_super_redrain_2
+jd_syj
+jd_npc_redrain
+jd_daily_lottery
 "
 
 #添加需要添加脚本的 cron
@@ -40,8 +43,9 @@ jd_super_redrain="0 0-23/1 * * * jd jd_super_redrain"
 jd_half_redrain="30 20-23/1 * * * jd jd_half_redrain"
 jd_unsubscribe_2="45 22 * * * jd jd_unsubscribe_2"
 jd_adolf_ETIP="15 8 9-31 5 * jd jd_adolf_ETIP"
-jd_syj_2="10 0,7,23 * * * jd jd_syj_2"
-jd_super_redrain_2="0 0-23/1 * * * jd jd_super_redrain_2"
+jd_syj="10 0,7,23 * * * jd jd_syj"
+jd_npc_redrain="0 0-23/1 * * * jd jd_npc_redrain"
+jd_daily_lottery="13 1,22,23 * * * jd jd_daily_lottery"
 
 
 #添加需要删除的脚本的 name
@@ -57,7 +61,25 @@ for npc_scripts in $my_scripts_list_add
           sc_result=$(cat /jd/config/crontab.list | grep "${npc_scripts}")
 
             if [[ "$sc_result" != "" ]];then
-                echo -e "${npc_scripts} 脚本计划任务已存在，无需更新计划任务～ \n"
+                echo -e "${npc_scripts} 脚本计划任务已存在，检查是否需要更新计划任务～ \n"
+                
+                #取行号
+                line_id=`sed -n "/${npc_scripts}/=" ${my_cron_file}`
+                old_cron=`cat $my_cron_file | grep "$npc_scripts"`
+                eval new_cron=\${${npc_scripts}}
+
+                
+                #判断是否需要更新计划任务
+                if [ "$old_cron" = "$new_cron" ];then       
+                    echo -e "${npc_scripts} 新旧计划任务相同，无需更新～ \n"
+                else
+                    echo -e "正在更新 ${npc_scripts} 脚本计划任务 ～ \n"
+                    sed -i "${line_id} d" ${my_cron_file}
+                    sed -i "${line_id} i ${new_cron}" $my_cron_file
+                    crontab /jd/config/crontab.list
+                    echo -e " ${npc_scripts} 脚本计划任务更新完成 ～ \n"
+                
+                fi
             else
                 echo -e "${npc_scripts} 脚本计划任务不存在，准备更新～ \n"
                 eval npc_cron=\${${npc_scripts}}
