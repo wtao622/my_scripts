252e6ca4012abffbebb2828ec2b65484259f32cd
lpssxs <731588359@qq.com>
2021-05-11 23:45:13 +0800

    test4

--- shell/jd/lpss_diy_test.sh
+++ shell/jd/lpss_diy_test.sh
@@ -11,13 +11,15 @@
 
 # 下载需要添加的脚本 link 
 
-#wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/js_code/sendNotify_5.js -O /jd/sendNotify.js 
+#wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/html/home.html  -O /jd/home.html
+#wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/js_code/sendNotify_7.js -O /jd/sendNotify.js 
 wget -q --no-check-certificate  https://raw.githubusercontent.com/forpw2009/my_scripts/main/js_code/jd_unsubscribe_2.js -O /jd/scripts/jd_unsubscribe_2.js
 wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/js_code/jd/jd_super_redrain.js -O /jd/scripts/jd_super_redrain.js
 wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/js_code/jd/jd_half_redrain.js -O /jd/scripts/jd_half_redrain.js
 wget -q --no-check-certificate https://raw.githubusercontent.com/monk-coder/dust/dust/car/adolf_ETIP.js -O /jd/scripts/jd_adolf_ETIP.js
 wget -q --no-check-certificate https://jdsharedresourcescdn.azureedge.net/jdresource/jd_syj.js -O /jd/scripts/jd_syj.js 
 wget -q --no-check-certificate https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js  -O /jd/scripts/jd_npc_redrain.js 
+wget -q --no-check-certificate https://raw.githubusercontent.com/forpw2009/my_scripts/main/js_code/jd/jd_daily_lottery.js -O /jd/scripts/jd_daily_lottery.js 
 
 
 my_cron_file="/jd/config/crontab.list"
@@ -32,6 +34,7 @@ jd_unsubscribe_2
 jd_adolf_ETIP
 jd_syj
 jd_npc_redrain
+jd_daily_lottery
 "
 
 #添加需要添加脚本的 cron
@@ -42,6 +45,7 @@ jd_unsubscribe_2="45 22 * * * jd jd_unsubscribe_2"
 jd_adolf_ETIP="15 8 9-31 5 * jd jd_adolf_ETIP"
 jd_syj="10 0,7,23 * * * jd jd_syj"
 jd_npc_redrain="0 0-23/1 * * * jd jd_npc_redrain"
+jd_daily_lottery="13 1,22,23 * * * jd jd_daily_lottery"
 
 
 #添加需要删除的脚本的 name
