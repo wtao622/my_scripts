04b8909ccc319d615a2ff9f4e42048a39203360d
lpssxs <731588359@qq.com>
2021-05-12 23:04:16 +0800

    finish

--- shell/jd/jd_try.sh
+++ shell/jd/jd_try.sh
@@ -2,7 +2,7 @@
 
 # 脚本作者 ：  lpssxs
 # 更新时间 ：  2021/5/12
-# 主要功能 ： 更新 jd_try 项目ck
+# 主要功能 ： 更新 jd_try 项目 cookies
 
 
 
@@ -14,23 +14,22 @@ my_config_file="/jd/config/config.sh"
 
 sed -i '/Cookie/d' $my_config_file
 
-my_ck_list=`cat "$my_ck_file"`
 
 line_id=20
 
 while read line || [[ -n ${line} ]] 
 
 	do
-		  echo "正在插入以下内容 ：${line}"
+		  #echo "正在插入以下内容 ：${line}"
 
 		  sed -i "${line_id} i ${line}" $my_config_file
 		  
 		  line_id=$(($line_id+1))
 		  
-		  echo $line_id
+		  #echo $line_id
 		  
 	done < $my_ck_file
 
 
-echo "jd_try ck 更新成功"
+echo "jd_try cookies 更新成功~"
 
