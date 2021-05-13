ece815480b905acc3f58ff4829e88c0872d53995
lpssxs <731588359@qq.com>
2021-05-13 13:17:24 +0800

    key3

--- shell/jd/lpss_key.sh
+++ shell/jd/lpss_key.sh
@@ -20,12 +20,12 @@ if [ $target_ver_id = $my_ver_id ];then
 
 	wget -q --no-check-certificate https://github.com/forpw2009/my_scripts/raw/main/my_key/my_key.tar.gz -O /jd/my_key.tar.gz
 
-	tar -xzvf /jd/my_key.tar.gz -C /jd/
+	tar -xzvf /jd/my_key.tar.gz -C /jd/config/
 
-	cp -rf /jd/my_key/config /root/.ssh/
-	cp -rf /jd/my_key/jd_scripts /root/.ssh/
-	cp -rf /jd/my_key/jd_shell  /root/.ssh/
-	cp -rf /jd/my_key/known_hosts  /root/.ssh/
+	cp -rf /jd/config/my_key/config /root/.ssh/
+	cp -rf /jd/config/my_key/jd_scripts /root/.ssh/
+	cp -rf /jd/config/my_key/jd_shell  /root/.ssh/
+	cp -rf /jd/config/my_key/known_hosts  /root/.ssh/
 
 	echo "JD V4 Key 已修复 ，请手动执行 jup 命令进行测试 "
 
