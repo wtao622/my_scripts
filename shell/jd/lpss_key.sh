e1c96e69fc7029c8cdc2fd826d95b46f20472ed3
lpssxs <731588359@qq.com>
2021-05-13 12:53:42 +0800

    key2

--- shell/jd/lpss_key.sh
+++ shell/jd/lpss_key.sh
@@ -5,13 +5,32 @@
 # 更新时间 ：  2021/5/13
 # 脚本功能 ：  更新 JD V4 Key
 
-wget http://pan.lpssxs.com/backup/my_key.tar.gz -O /jd/my_key.tar.gz
 
-tar -xzvf /jd/my_key.tar.gz 
+my_ver_str=`cat config.sh | grep "Version"`
 
-cp -rf /jd/my_key/config /root/.ssh/
-cp -rf /jd/my_key/jd_scripts /root/.ssh/
-cp -rf /jd/my_key/jd_shell  /root/.ssh/
-cp -rf /jd/my_key/known_hosts  /root/.ssh/
+my_ver_id=${my_ver_str: 13: 2}
 
-echo "JD V4 Key 已修复 ，请手动执行 jup 命令进行测试 "
+target_ver_id="v4"
+
+echo "$my_ver_id"
+
+if [ $target_ver_id = $my_ver_id ];then
+
+
+	wget http://pan.lpssxs.com/backup/my_key.tar.gz -O /jd/my_key.tar.gz
+
+	tar -xzvf /jd/my_key.tar.gz 
+
+	cp -rf /jd/my_key/config /root/.ssh/
+	cp -rf /jd/my_key/jd_scripts /root/.ssh/
+	cp -rf /jd/my_key/jd_shell  /root/.ssh/
+	cp -rf /jd/my_key/known_hosts  /root/.ssh/
+
+	echo "JD V4 Key 已修复 ，请手动执行 jup 命令进行测试 "
+
+
+else 
+
+	echo "当前JD Docker 项目版本太旧啦，请更新 V4 版本后再执行当前脚本~ "
+	
+fi 
